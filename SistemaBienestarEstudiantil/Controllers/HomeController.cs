82ba528d543e4b28fd4ce38da651f3cf583ee19d
Michel Quintosa <micheljqh@gmail.com>
2016-02-21 17:25:18 -0500

    fisrt commit

---
+++ SistemaBienestarEstudiantil/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaBienestarEstudiantil.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "ASP.NET MVC";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        public ActionResult Roles()
+        {
+            return View();
+        }
+
+        public ActionResult Datos()
+        {
+            return View();
+        }
+
+        public ActionResult Tareas()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaBienestarEstudiantil/Views/Home/Roles.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+<asp:Content ID="aboutTitle" ContentPlaceHolderID="TitleContent" runat="server">
+    Acerca de nosotros
+</asp:Content>
+
+<asp:Content ID="aboutContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Roles</h2>
+    <p>
+        Incluya aquí el contenido.
+    </p>
+</asp:Content>
+
