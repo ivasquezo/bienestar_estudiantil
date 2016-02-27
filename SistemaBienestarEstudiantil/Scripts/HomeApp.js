a6cd69f99dfdfc8b0cb91709da8ede75768c9d50
Michel Quintosa <mquintosa@kruger.com.ec>
2016-02-27 16:40:16 -0500

    added entity models

--- SistemaBienestarEstudiantil/Scripts/HomeApp.js
+++ SistemaBienestarEstudiantil/Scripts/HomeApp.js
@@ -5,13 +5,26 @@
         $scope.Message = "Mensaje controller funcionando";
         $scope.prueba = "";
     });
-    app.controller('UsuarioController', function ($scope) {
-        $scope.Message = "Usuarios controller funcionando";
-    });
+
     app.controller('DatosController', function ($scope) {
         $scope.Message = "DAtos controller funcionando";
     });
+
     app.controller('TareasController', function ($scope) {
         $scope.Message = "Tareas Mensaje controller funcionando";
     });
+
+    app.controller('UsuarioController', ['$scope', '$http', function ($scope, $http) {
+        $scope.Message = "Tareas Mensaje controller funcionando";
+        $scope.cargarUsuarios = function () {
+            $http.post('../../WebServices/Users.asmx/allUser', {
+            }).success(function (data, status, headers, config) {
+                $scope.users = data;
+            }).error(function (data, status, headers, config) {
+                console.log("error al cargar los usuarios");
+            });
+        };
+
+        $scope.cargarUsuarios();
+    } ]);
 })();
--- SistemaBienestarEstudiantil/Views/Home/Usuarios.aspx
+++ SistemaBienestarEstudiantil/Views/Home/Usuarios.aspx
@@ -4,9 +4,20 @@
     Acerca de nosotros
 </asp:Content>
 <asp:Content ID="aboutContent" ContentPlaceHolderID="MainContent" runat="server">
-    <%--<div ng-controller="UsuarioController">
-        {{Message}}
-    </div>--%>
+    <div ng-controller="UsuarioController">
+        <table>
+            <tr>
+                <td>Código</td>
+                <td>Nombre</td>
+                <td>Estado</td>
+            </tr>
+            <tr ng-repeat="user in users">
+                <td>{{user.CODIGOUSUARIO}}</td>
+                <td>{{user.NOMBREUSUARIO}}</td>
+                <td>{{user.ESTADOUSUARIO}}</td>
+            </tr>
+        </table>
+    </div>
     <p>
         <input type="submit" value="Iniciar sesión" style="float:right" />
     </p>
